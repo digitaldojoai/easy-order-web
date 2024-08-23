a22104595d64ffd1050fe8732156ce44ebb4b980
hadi-makki <165915385+hadi-makki@users.noreply.github.com>
2024-08-23 13:24:21 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/login");
+  });
   return (
     <div className="flex gap-4">
       <Sidebar className="h-screen">
